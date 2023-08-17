f2f47241550b22aec76fc429639b9be654894a13
n0zmu <mickael.munozserrano@gmail.com>
2023-08-17 14:06:42 +0200

    dmenu col

--- config.def.h
+++ config.def.h
@@ -61,7 +61,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-p", "Run:", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_cyan, "-sb", col_gray3, "-sf", col_gray1, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-p", "Run:", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_cyan, "-sb", col_cyan, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browscmd[] = { "chromium", NULL};
 static const char *lockcmd[]  = { "slock",  NULL };
